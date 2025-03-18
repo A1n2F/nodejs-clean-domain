f2e5f78297bb1cfb79964d514023ec855210e770
A1n2F <alissonfferreira22@gmail.com>
2025-03-18 14:26:07 -0300

    Fórum com DDD & Clean Architecture - Refatorando testes unitários

---
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -0,0 +1,24 @@
+import { expect, describe, beforeEach, it } from "vitest"
+import { AnswerQuestionUseCase } from "./answer-question"
+import { InMemoryAnswersRepository } from "test/repositories/in-memory-answer-repository"
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: AnswerQuestionUseCase
+
+describe("Create Answer", () => {
+    beforeEach(() => {
+        inMemoryAnswersRepository = new InMemoryAnswersRepository()
+        sut = new AnswerQuestionUseCase(inMemoryAnswersRepository)
+    })
+    
+    it("should be able to create an answer", async () => {    
+        const { answer } = await sut.execute({
+            questionId: "1",
+            instructorId: "1",
+            content: "Conteúdo da resposta"
+        })
+    
+        expect(answer.id).toBeTruthy()
+        expect(inMemoryAnswersRepository.items[0].id).toEqual(answer.id)
+    })
+})
