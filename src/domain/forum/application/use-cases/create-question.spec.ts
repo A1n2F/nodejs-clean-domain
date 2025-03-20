d6e9becff07a3235c8a16f4c2f0c47581ae1aadc
A1n2F <alissonfferreira22@gmail.com>
2025-03-20 14:39:26 -0300

    Aggregates & Watched Lists - Criando pergunta com anexos

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,6 +1,7 @@
 import { expect, describe, beforeEach, it } from "vitest"
 import { CreateQuestionUseCase } from "./create-question"
 import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository"
+import { UniqueEntityID } from "src/core/entities/unique-entity-id"
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let sut: CreateQuestionUseCase
@@ -15,10 +16,16 @@ describe("Create Question", () => {
         const result = await sut.execute({
             authorId: "1",
             title: "Nova pergunta",
-            content: "Conteúdo da pergunta"
+            content: "Conteúdo da pergunta",
+            attachmentsIds: ["1", "2"]
         })
     
         expect(result.isRight()).toBe(true)
         expect(inMemoryQuestionsRepository.items[0]).toEqual(result.value?.question)
+        expect(inMemoryQuestionsRepository.items[0].attachments).toHaveLength(2)
+        expect(inMemoryQuestionsRepository.items[0].attachments).toEqual([
+            expect.objectContaining({ attachmentId: new UniqueEntityID("1")}),
+            expect.objectContaining({ attachmentId: new UniqueEntityID("2")})
+        ])
     })
 })
