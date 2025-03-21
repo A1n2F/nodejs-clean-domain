2d3de4d0bd322797f2a80b5f00dc93e78030fb19
A1n2F <alissonfferreira22@gmail.com>
2025-03-21 12:36:34 -0300

    Subdomínios & Domain Events - Evento: Resposta criada

--- src/domain/forum/enterprise/entities/answer.ts
+++ src/domain/forum/enterprise/entities/answer.ts
@@ -1,7 +1,8 @@
-import { Entity } from "../../../../core/entities/entity"
+import { AggregateRoot } from "src/core/entities/aggregate-root"
 import { UniqueEntityID } from "../../../../core/entities/unique-entity-id"
 import { Optional } from "../../../../core/types/optional"
 import { AnswerAttachmentList } from "./answer-attachment-list"
+import { AnswerCreatedEvent } from "../events/answer-created-event"
 
 export interface AnswerProps {
     authorId: UniqueEntityID
@@ -12,7 +13,7 @@ export interface AnswerProps {
     updateAt?: Date
 }
 
-export class Answer extends Entity<AnswerProps> {
+export class Answer extends AggregateRoot<AnswerProps> {
     get authorId() {
         return this.props.authorId
     }
@@ -61,6 +62,12 @@ export class Answer extends Entity<AnswerProps> {
             attachments: props.attachments ?? new AnswerAttachmentList(),
             createdAt: props.createdAt ?? new Date(),
         }, id)
+
+        const isNewAnswer = !id
+
+        if (isNewAnswer) {
+            answer.addDomainEvent(new AnswerCreatedEvent(answer))
+        }
 
         return answer
     }
